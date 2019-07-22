ca22ad180ba1d9b38c2481a41a96c4a7f9ca9dd6
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2019-07-22 19:14:48 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here
+let var1 = 62;
+
+let var2 = 1;
+
+let multiply = var1 * var2;
+
+let random = Math.ceil(Math.random());
+
+let mod = 67 % 7;
+
+let max = Math.max(1, 3, 4, 20, 3);
