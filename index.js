14c12baf91e532bc5792ea39d698e2579cd7502d
LaRaye <lnj2105@gmail.com>
2019-08-19 20:35:57 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here
+let var1 = 1;
+let var2 = 62;
+const multiply = var1 * var2;
+
+const random = Math.ceil(Math.random() * 100 + 1);
+
+const mod = 19 % 5;
+
+const max = Math.max(3, 18, 20, 6);
